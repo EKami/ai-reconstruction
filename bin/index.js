512b1abc1b43fd5e5385d9ebfa7917459a798256
Tuatini Godard <email@tuatini.me>
2021-07-07 11:27:25 +0200

    Fix args

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,19 @@
 #!/usr/bin/env node
 
+const fs = require('fs');
 const yargs = require("yargs");
+const sharp = require('sharp');
+
+function main(args) {
+    const positions = JSON.parse(fs.readFileSync(args.patches_positions));
+    const max_thumb_size = args.max_thumb_size
+    const output_file = args.output_file
+    const wsi = options.wsi
+
+    console.log(`Using: ${args.wsi}!`);
+    sharp(wsi).resize(max_thumb_size).png().toFile(output_file)
+    console.log(`Output available in ${output_file}`)
+}
 
 const options = yargs
     .usage("Given a WSI as input and patche positions, outputs a WSI thumbnail with its overlay")
@@ -10,4 +23,5 @@ const options = yargs
     .option("output_file", {describe: "The output file for the WSI + overlay image", type: "string", demandOption: true})
     .argv;
 
-console.log(`Using: ${options.wsi}!`);
+main(options)
+
