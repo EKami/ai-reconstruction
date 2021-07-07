7a91805bd528f1a3d75c173c907bdd2e92a1ab83
Tuatini Godard <email@tuatini.me>
2021-07-07 12:27:41 +0200

    Add thumbnail size calculation

--- bin/index.js
+++ bin/index.js
@@ -5,13 +5,19 @@ const yargs = require("yargs");
 const sharp = require('sharp');
 
 function main(args) {
-    const positions = JSON.parse(fs.readFileSync(args.patches_positions));
+    const patches_positions = JSON.parse(fs.readFileSync(args.patches_positions));
     const max_thumb_size = args.max_thumb_size
     const output_file = args.output_file
     const wsi = options.wsi
 
+    const full_size_dim = patches_positions["wsi_full_resolution"]
+    const width_ratio = full_size_dim[0] / thumb_pyvips_img.width
+    const height_ratio = full_size_dim[1] / thumb_pyvips_img.height
+
     console.log(`Using: ${args.wsi}!`);
     sharp(wsi).resize(max_thumb_size).png().toFile(output_file)
+
+
     console.log(`Output available in ${output_file}`)
 }
 
